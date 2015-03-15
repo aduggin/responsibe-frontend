2b7789beb904da481a925e40e045da3525facfa7
Alistair Duggin <alistair.duggin@btinternet.com>
2015-03-15 10:08:15 +0000

    Add handlebars and example partials and layout

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.engine('html', exphbs({defaultLayout: 'main', extname: 'html'}));
+app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Responsible Frontend' });
 });
 
 app.listen(app.get('port'), function() {
