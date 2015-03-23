eee31661ed68c5977cd8b6f2adbb82d2528305d8
Alistair Duggin <alistair.duggin@btinternet.com>
2015-03-23 10:50:50 +0000

    Test htmlinspector

---
+++ html-inspector-config.js
@@ -0,0 +1,12 @@
+function onComplete(reports) {
+  if (reports.length) {
+    throw new Error("There was an error - fail the build!");
+  }
+}
+
+
+HTMLInspector.inspect({
+  excludeRules: ["unused-classes"],
+  excludeElements: ["svg", "iframe"]
+  //, onComplete: onComplete
+});
